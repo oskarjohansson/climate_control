a2709fb3921f983fe4cc34bd4f16c6d2f202b048
Oskar Johansson <oskar@irisnet.se>
2021-04-14 13:14:05 +0200

    Added SOC led blinks.
    The LED will now blink according to the return of getSoc.
    There are three levels,
    0% - 40%, 40% - 70% and 70% - 100%.
    One blink is lowest and three is full.

--- software/climate_control/src/constants.h
+++ software/climate_control/src/constants.h
@@ -15,11 +15,15 @@ const int groupClickInterval = 300;
 const int longClickDuration = 1000;
 const int inactiveTime = 5000;
 
+const unsigned long socLedTimestep = 400;   // Timestep
+const unsigned long socLedCycle = 5000;     // How often the soc should cycle
+const uint8_t socLedPattern[3][5] = {{0,1,1,1,1},{0,1,0,1,1},{0,1,0,1,0}}; // The pattern for the three battery levels
+
 const float voltPerInc = 0.0062; // Magic number ;-) 1.1 * ((100 + 30) / 30) / 1024; // internal_ref * voltage_division⁽⁻¹⁾ / resolution
 const int underVoltThreshold = 447; //2.8 / voltPerInc;
 
-const int batSocVoltageLevels = 2;
-const unsigned int batSocVoltage[] = {535, 624};
+const int batSocVoltageLevels = 3;
+const unsigned int batSocVoltage[] = {underVoltThreshold, 535, 624};
                                       /*{(unsigned int)(3.48 / voltPerInc),  // ~20%
                                       (unsigned int)(3.60 / voltPerInc),  // ~40%
                                       (unsigned int)(3.71 / voltPerInc),  // ~60%
--- software/climate_control/src/main.cpp
+++ software/climate_control/src/main.cpp
@@ -16,6 +16,9 @@ TODO: Verify heater function
 Button switchButton(switchPin);
 Heater heater(heaterPwmPin, boostEnablePin, ledPin);
 
+// For battery soc led
+uint8_t queueIndex = 0;
+unsigned long socLastCycle = 5000;
 
 void setup()
 {
@@ -30,44 +33,51 @@ void setup()
 
 void loop()
 {
-  if (getSoc())
+  unsigned long now = millis();
+  // Read battery status
+  uint8_t Soc = getSoc();
+  if (Soc)
   {
     // Check if button is pressed
     int click = switchButton.poll();
     switch (click)
     {
-    case clickTypes::singleClick:
-      Serial.println("Single click detected!");
-      if (heater.isActive())
-        heater.increment();
-      break;
-    case clickTypes::doubleClick:
-      Serial.println("Double click detected!");
-      if (heater.isActive())
-        heater.decrement();
-      break;
-    case clickTypes::tripleClick:
-    { // Get battery status
-      Serial.println("Triple click detected!");
-      //heater.pause();
-      //delay(10);
-      //uint8_t soc = getSoc();
-      //flash(ledPin, soc+1, 500, 250);
-      //heater.resume();
-      break;
-    }
-    case clickTypes::longClick:
-      if (heater.isActive())
-      {
-        Serial.println("Turning off heater!");
-        heater.off();
+      case clickTypes::singleClick:
+        Serial.println("Single click detected!");
+        socLastCycle = now + socLedCycle;
+        if (heater.isActive())
+          heater.increment();
+        break;
+      case clickTypes::doubleClick:
+        Serial.println("Double click detected!");
+        socLastCycle = now + socLedCycle;
+        if (heater.isActive())
+          heater.decrement();
+        break;
+      case clickTypes::tripleClick:
+      { // Get battery status
+        Serial.println("Triple click detected!");
+        socLastCycle = now + socLedCycle;
+        //heater.pause();
+        //delay(10);
+        //uint8_t soc = getSoc();
+        //flash(ledPin, soc+1, 500, 250);
+        //heater.resume();
+        break;
       }
-      else
-      {
-        Serial.println("Turning on heater!");
-        heater.on();
-      }
-      break;
+      case clickTypes::longClick:
+        if (heater.isActive())
+        {
+          Serial.println("Turning off heater!");
+          heater.off();
+        }
+        else
+        {
+          Serial.println("Turning on heater!");
+          heater.on();
+        }
+        break;
+      
     }
     // If neither heater active nor switchbutton active
     if (!(switchButton.isActive() || heater.isActive()))
@@ -76,6 +86,38 @@ void loop()
       delay(25);
       goToSleep();
     }
+
+    // Blink battery level led
+    else if((now>socLastCycle))
+    {
+      uint8_t ledMode = socLedPattern[Soc-1][queueIndex];
+      if(ledMode) 
+      {        
+        pinMode(ledPin, OUTPUT);
+        digitalWrite(ledPin, LOW);
+      }
+      else 
+      {
+        pinMode(ledPin, INPUT);
+      }
+
+      queueIndex = (queueIndex + 1) % (sizeof(socLedPattern[Soc])+1);
+      // If queueIndex is zero, wait another 5s
+      if(queueIndex<1){
+        socLastCycle = now + socLedCycle;
+        pinMode(ledPin, OUTPUT);
+        digitalWrite(ledPin, LOW);
+      }
+      else 
+      {
+        socLastCycle = now + socLedTimestep;
+      }
+    }
+
+
+
+
+
   }
   else
   {
